729144a5a6086c0f697fe444dcb25d3012d8d9e2
salonidhatrak <salonidhatrak123@gmail.com>
2023-09-08 15:34:18 +0530

    css checked

--- cryptostore/src/App.js
+++ cryptostore/src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
-
 import { useEffect, useState } from "react";
 import Axios from "axios";
 import Crypto from './components/Crypto';
 
 function App() {
-const [listOfCoins, setListOfCoins] = useState([]);
-const [searchWord, setSearchWord] = useState("");
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
 
-useEffect(() => {
-  Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
-    (response) => {
-      setListOfCoins(response.data.coins);
-    }
-  );
-}, []);
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+      (response) => {
+        setListOfCoins(response.data.coins);
+      }
+    );
+  }, []);
 
-const filteredCoins = listOfCoins.filter((coin) => {
-  return coin.name.toLowerCase().includes(searchWord.toLowerCase());
-});
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
 
   return (
     <div className="App">
+      <h1>Crypto Purchase Interface</h1>
       <div className="cryptoHeader">
         <input
           type="text"
